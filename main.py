08d922e324517d9e478ba3dfc511319fe10fec35
boss0703 <prinnykurtis002@gmail.com>
2021-07-28 10:29:19 +0900

    add Redmine window

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import time
 
 from selenium import webdriver
 
-from local_settings import DRIVER_PATH, PASSWORD, EMAIL, MANABY_ID, MANABY_PASSWORD
-
+from local_settings import DRIVER_PATH, PASSWORD, EMAIL, MANABY_ID, MANABY_PASSWORD, REDMINE_ID
 
 try:
     options = webdriver.ChromeOptions()
@@ -23,16 +22,26 @@ try:
     driver.find_element_by_id('action-form').find_element_by_name('password').send_keys(PASSWORD)
     driver.find_element_by_id('submit-button').click()
 
-    print('open sushida...')
+    print('open Redmine and login...')
     driver.execute_script('window.open()')
     window = driver.window_handles
     driver.switch_to.window(window[1])
+    driver.get('http://ec2-54-199-63-60.ap-northeast-1.compute.amazonaws.com/login')
+    driver.find_element_by_id('username').send_keys(REDMINE_ID)
+    driver.find_element_by_id('password').send_keys(PASSWORD)
+    driver.find_element_by_id('login-submit').click()
+    driver.get('http://ec2-54-199-63-60.ap-northeast-1.compute.amazonaws.com/projects/homete/issues/gantt')
+
+    print('open sushida...')
+    driver.execute_script('window.open()')
+    window = driver.window_handles
+    driver.switch_to.window(window[2])
     driver.get('http://typingx0.net/sushida/play.html')
 
     print('open shakyo.io and login...')
     driver.execute_script('window.open()')
     window = driver.window_handles
-    driver.switch_to.window(window[2])
+    driver.switch_to.window(window[3])
     driver.get('https://shakyo.io/users/sign_in')
     driver.find_element_by_id('user_email').send_keys(EMAIL)
     driver.find_element_by_id('user_password').send_keys(PASSWORD)
@@ -42,7 +51,7 @@ try:
     print('open manaby e-learning site and login...')
     driver.execute_script('window.open()')
     window = driver.window_handles
-    driver.switch_to.window(window[3])
+    driver.switch_to.window(window[4])
     driver.get('https://elearning-site.herokuapp.com/top/manaby')
     driver.find_element_by_id('user_usernumber').send_keys(MANABY_ID)
     driver.find_element_by_id('user_password').send_keys(MANABY_PASSWORD)
